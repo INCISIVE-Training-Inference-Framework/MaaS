6d449b84ffb46a50a302cd2704c08c8aeabae368
Marc Palacín <19353116+ProtossGP32@users.noreply.github.com>
2024-02-29 15:59:54 +0100

    Add Swagger auto-schema generation (#8)
    * feat: add swagger API modules
    
    * upd: update Docker compose files for Swagger
    
    * upd: add docker compose healthcheck
    
    * upd: update README.md with API docs instructions
    
    ---------
    
    Co-authored-by: Marc Palacín Marfil <marc.palacin@bsc.es>

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from main.api import views
 
@@ -19,4 +20,9 @@ For nested representations check:
 urlpatterns = [
     path(r'', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    # SPECTACULAR URLS
+    path('api/schemas/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schemas/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schemas'), name='swagger-ui'),
+    path('api/schemas/redoc/', SpectacularRedocView.as_view(url_name='schemas'), name='redoc')
 ]
---
+++ docker_compose_init_maas.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Script to initialize the local db and run the django server
+echo "Migrating database (python3 manage.py migrate)"
+python3 manage.py migrate
+python3 manage.py spectacular --color --file /swagger/schema.yml --validate
+python3 manage.py collectstatic --clear --noinput
+echo "Initializing gunicorn..."
+gunicorn --bind :8000 --workers 3 maas.wsgi:application
